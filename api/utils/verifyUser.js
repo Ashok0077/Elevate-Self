b30cf20d56944ef848ee086e96d46e72a893c7da
Tapan <tapansonak786@gmail.com>
2024-03-18 19:25:59 +0530

    commit

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -4,16 +4,16 @@ export const verifyToken = (req, res, next) => {
   console.log(req);
   const token = req.cookies.access_token;
   console.log("Token - ", token);
-  // res.json({
-  //   method: req.method,
-  //   url: req.url,
-  //   headers: req.headers,
-  //   cookies: req.cookies,
-  //   params: req.params,
-  //   query: req.query,
-  //   body: req.body,
-  //   token: token,
-  // });
+  res.json({
+    method: req.method,
+    url: req.url,
+    headers: req.headers,
+    cookies: req.cookies,
+    params: req.params,
+    query: req.query,
+    body: req.body,
+    token: token,
+  });
   if (token === undefined) {
     return next(
       errorHandler(
