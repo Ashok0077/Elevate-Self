1d4f6a482c220d881905149dcbc701ab04998c4a
Tapan <tapansonak786@gmail.com>
2024-03-18 18:47:40 +0530

    commit

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -4,7 +4,7 @@ export const verifyToken = (req, res, next) => {
   console.log(req);
   const token = req.cookies.access_token;
   console.log("Token - ", token);
-  res.json({ token });
+  res.json({ req: req, token: token });
   // if (token === undefined) {
   //   return next(errorHandler(401, "Unauthorized: Token is missing"));
   // }
