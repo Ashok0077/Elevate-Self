a4f023dc06a992e00044119085a85b6e92443b5a
Tapan <tapansonak786@gmail.com>
2024-03-18 18:42:05 +0530

    commit

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,12 +1,13 @@
 import jwt from "jsonwebtoken";
 import { errorHandler } from "./error.js";
 export const verifyToken = (req, res, next) => {
+  console.log(req);
   const token = req.cookies.access_token;
   console.log("Token - ", token);
   // res.json(token);
-  if (token === undefined) {
-    return next(errorHandler(401, "Unauthorized: Token is missing"));
-  }
+  // if (token === undefined) {
+  //   return next(errorHandler(401, "Unauthorized: Token is missing"));
+  // }
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
       return next(errorHandler(401, "Unauthorized: Invalid or expired token"));
